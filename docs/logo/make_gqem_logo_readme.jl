38979eb51298c83392f433184eeaa9defdd7e85a
itsdfish <9677184+itsdfish@users.noreply.github.com>
2024-10-06 20:23:22 +0000

    Format .jl files

--- docs/logo/make_gqem_logo_readme.jl
+++ docs/logo/make_gqem_logo_readme.jl
@@ -15,14 +15,13 @@ linewidth = 3
 
 origin_point = Point(-1500, 15)
 
-b1 = [1,0]
-b1′ = [0,-1]
+b1 = [1, 0]
+b1′ = [0, -1]
 
-b2 = 𝕦(π/4) * b1
-b2′ = 𝕦(π/4) * b1′
+b2 = 𝕦(π / 4) * b1
+b2′ = 𝕦(π / 4) * b1′
 
-b3 = 𝕦(-.4) * b1
-
+b3 = 𝕦(-0.4) * b1
 
 line1_end = Point(b1 * r...) + origin_point
 line2_end = Point(b1′ * r...) + origin_point
@@ -31,7 +30,6 @@ line3_end = Point(b2 * r...) + origin_point
 line4_end = Point(b2′ * r...) + origin_point
 
 line5_end = Point(b3 * r...) + origin_point
-
 
 # sethue((0.251, 0.388, 0.847))
 sethue("black")
@@ -48,7 +46,6 @@ sethue((0.251, 0.388, 0.847))
 arrow(origin_point, line2_end; linewidth, arrowheadlength)
 strokepath()
 
-
 # line 3
 sethue((0.584, 0.345, 0.698))
 arrow(origin_point, line3_end; linewidth, arrowheadlength)
@@ -62,11 +59,10 @@ sethue((0.796, 0.235, 0.200))
 arrow(origin_point, line5_end; linewidth, arrowheadlength)
 strokepath()
 
-
 # text
 sethue("black")
 fontsize(220)
-text("Quantum Episodic Memory", Point(-1500+300, 70))
+text("Quantum Episodic Memory", Point(-1500 + 300, 70))
 
 finish()
 preview()
