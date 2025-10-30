6ecf7538ef1f1935be6be276ef110aab9a76a32d
itsdfish <itsdfish@gmail.com>
2025-10-30 03:04:31 -0400

    update to PrettyTables 3

--- src/utilities.jl
+++ src/utilities.jl
@@ -1,8 +1,4 @@
 function Base.show(io::IO, ::MIME"text/plain", model::AbstractGQEM)
-    return _show(io::IO, model)
-end
-
-function _show(io::IO, model)
     values = [getfield(model, f) for f in fieldnames(typeof(model))]
     values = map(x -> typeof(x) == Bool ? string(x) : x, values)
     T = typeof(model)
@@ -11,12 +7,12 @@ function _show(io::IO, model)
         io,
         values;
         title = model_name,
-        row_label_column_title = "Parameter",
+        column_labels = ["Value"],
+        stubhead_label = "Parameter",
         compact_printing = false,
-        header = ["Value"],
-        row_label_alignment = :l,
+        row_label_column_alignment = :l,
         row_labels = [fieldnames(typeof(model))...],
-        formatters = ft_printf("%5.2f"),
+        formatters = [fmt__printf("%5.2f", [2,])],
         alignment = :l
     )
 end
